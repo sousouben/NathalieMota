60f964539e14e23833c03e45c5eaf9310434e9eb
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2023-11-30 08:44:21 +0100

    Thème personnalisé

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php wp_footer() ?>
+<footer>
+    <div class=footer>
+        <ul>
+            <li><a href="#">MENTIONS LÉGALES</a></li>
+            <li><a href="#">VIE PRIVÉE</a></li>
+            <li><p>TOUS DROITS RÉSERVÉS</p></li>
+        </ul>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+<div class="content">
+    <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+    </div>
+<?php get_footer() ?>
