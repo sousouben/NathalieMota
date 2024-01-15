3919c97395ba35ce0972f3dba7538a8abe8394e9
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2024-01-15 11:13:32 +0100

    Intégration de la lightbox et responsive

--- js/contact-modale.js
+++ js/contact-modale.js
@@ -1,28 +1,34 @@
-console.log("modale ok");
+// Sélection des éléments du DOM
+const MODAL_OVERLAY = $(".bg_modale");
+const referencePhoto = $("#ref");
+const boutonContact = $("#boutonContact");
 
+// Fonction pour ouvrir la modale
+const openModal = () => {
+  // Affichage de la modale en utilisant Flexbox
+  MODAL_OVERLAY.css("display", "flex");
+  if (
+    boutonContact.attr("data-reference") &&
+    boutonContact.attr("data-reference").trim() !== ""
+  ) {
+    referencePhoto.val(boutonContact.attr("data-reference"));
+  }
+};
+
+// Fonction pour fermer la modale
+const closeModal = () => {
+  MODAL_OVERLAY.css("display", "none");
+};
+
+// Gestionnaire d'événements pour l'ouverture de la modale sur la page de contact
+if (window.location.pathname === "/contact/") {
+  openModal();
+}
+
+// Reste du code inchangé
 $(document).ready(() => {
   // Sélection des éléments du DOM
   const CONTACT_LINK = $("#menu-item-40");
-  const MODAL_OVERLAY = $(".bg_modale");
-  const referencePhoto = $("#ref");
-  const boutonContact = $("#boutonContact");
-
-  // Fonction pour ouvrir la modale
-  const openModal = () => {
-    // Affichage de la modale en utilisant Flexbox
-    MODAL_OVERLAY.css("display", "flex");
-    if (
-      boutonContact.attr("data-reference") &&
-      boutonContact.attr("data-reference").trim() !== ""
-    ) {
-      referencePhoto.val(boutonContact.attr("data-reference"));
-    }
-  };
-
-  // Fonction pour fermer la modale
-  const closeModal = () => {
-    MODAL_OVERLAY.css("display", "none");
-  };
 
   // Gestionnaires d'événements pour l'ouverture de la modale
   CONTACT_LINK.on("click", (event) => {
--- js/hamburger.js
+++ js/hamburger.js
@@ -5,12 +5,14 @@ console.log("hamburger ok");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const crossIcon = document.querySelector(".cross-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
+const body = document.querySelector("body");
 
 // Fonction pour ouvrir le menu hamburger en version mobile
 function openMobileMenu() {
   mobileMenu.classList.add("open");
   hamburgerIcon.style.display = "none";
   crossIcon.style.display = "block";
+  body.style.overflow = "hidden";
 }
 
 // Fonction pour fermer le menu hamburger en version mobile
@@ -18,6 +20,7 @@ function closeMobileMenu() {
   mobileMenu.classList.remove("open");
   hamburgerIcon.style.display = "block";
   crossIcon.style.display = "none";
+  body.style.overflow = "scroll";
 }
 
 // Ouverture du menu au clic sur le hamburger
